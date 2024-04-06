0453d9071e4b701b2facf790e76ceeddecb3e366
duridev <duridev@outlook.com>
2024-04-06 20:31:07 -0300

    creacion de variables script2

---
+++ assets/js/script2.js
@@ -0,0 +1,15 @@
+
+// * Variables
+
+const rojo = document.querySelector("#btn-1");
+const naranja = document.querySelector("#btn-2");
+const amarillo = document.querySelector("#btn-3");
+const verde = document.querySelector("#btn-4");
+const celeste = document.querySelector("#btn-5");
+const fuxia = document.querySelector("#btn-6");
+
+const caja = document.querySelector("#caja")
+
+
+
+
