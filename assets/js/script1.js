ddde8ccd64b43bd0edc342cc831b9097cf8bd3b2
duridev <duridev@outlook.com>
2024-04-06 13:04:23 -0300

    Variables en script 1

---
+++ assets/js/script1.js
@@ -0,0 +1,13 @@
+
+// * Variables
+
+
+const errorNombre = document.querySelector(".errorNombre");
+const errorAsunto = document.querySelector(".errorAsunto");
+const errorMensaje = document.querySelector(".errorMensaje");
+
+const form = document.querySelector("#formulario");
+const resultado = document.querySelector(".resultado");
+const enviar = document.querySelector("#botton");
+
+
