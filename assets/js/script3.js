fe95525821d8299629263b94a343f7f5409d2313
duridev <duridev@outlook.com>
2024-04-07 00:33:58 -0400

    variables script3

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -3,7 +3,8 @@
 
 const valor1 = document.querySelector("#valor1");
 const valor2 = document.querySelector("#valor2");
-const resultado = document.querySelector("#resultado")
+const resultado = document.querySelector(".resultado")
 
 const sumar = document.querySelector('#btn-sumar');
 const restar = document.querySelector('#btn-restar');
+
